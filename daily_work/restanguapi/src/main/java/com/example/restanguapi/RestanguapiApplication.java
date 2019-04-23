25f257848c48b7386e1789cc6912010227ac7c6e
sushmi5keka <sushmi5keka@gmail.com>
2019-04-23 13:34:37 +0600

    ok

---
+++ daily_work/restanguapi/src/main/java/com/example/restanguapi/RestanguapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restanguapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestanguapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestanguapiApplication.class, args);
+    }
+
+}
---
+++ daily_work/restanguapi/src/test/java/com/example/restanguapi/RestanguapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restanguapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestanguapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
